432bb32f9c2a358a952f906897608f2ec0d493d9
Андрей Поляков <avpgenium@yandex.ru>
2016-07-06 01:24:28 +0500

    hometask 4

--- app/src/main/java/com/softdesig/devintensive/data/managers/PreferenceManager.java
+++ app/src/main/java/com/softdesig/devintensive/data/managers/PreferenceManager.java
@@ -1,6 +1,7 @@
 package com.softdesig.devintensive.data.managers;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 
 import com.softdesig.devintensive.utils.ConstantManager;
 import com.softdesig.devintensive.utils.DevintensiveApplication;
@@ -35,5 +36,15 @@ public class PreferenceManager {
         userFields.add(mSharedPreferences.getString(ConstantManager.USER_GIT_KEY, "null"));
         userFields.add(mSharedPreferences.getString(ConstantManager.USER_ABOUT_KEY, "null"));
         return userFields;
+    }
+
+    public void saveUserPhoto(Uri uri){
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_PHOTO_KEY, uri.toString());
+        editor.apply();
+    }
+
+    public Uri loadUserPhoto(){
+        return Uri.parse(mSharedPreferences.getString(ConstantManager.USER_PHOTO_KEY, "android.resource://com.softdesign.devintensive/drawable/header_bg_1"));
     }
 }
